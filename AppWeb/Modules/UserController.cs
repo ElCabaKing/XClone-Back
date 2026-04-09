c1587fe7532d0ebbef8fbf50952eb6074530b383
ElCabaKing <davi.cabanilla@gmail.com>
2026-04-09 11:26:05 -0500

    init commit: this i the begining of a fancy code

---
+++ AppWeb/Modules/UserController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppWeb.Modules.User
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
