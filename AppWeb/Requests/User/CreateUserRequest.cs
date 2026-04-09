52a698717a6c283d905d0e0859b6635e569d18f2
ElCabaKing <davi.cabanilla@gmail.com>
2026-04-09 11:35:05 -0500

    lets go

---
+++ AppWeb/Requests/User/CreateUserRequest.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AppWeb.Requests.User;
+public class CreateUserRequest
+{
+    [Required(ErrorMessage = ValidationConstants.REQUIRED)]
+    [MaxLength(150, ErrorMessage = ValidationConstants.MAX_LENGTH)]
+    [MinLength(10, ErrorMessage = ValidationConstants.MIN_LENGTH)]
+    public string Username { get; set; } = default!;
+
+    [Required(ErrorMessage = ValidationConstants.REQUIRED)]
+    [EmailAddress(ErrorMessage = ValidationConstants.EMAIL_ADDRESS)]
+    public string Email { get; set; } = default!;
+    [Required(ErrorMessage = ValidationConstants.REQUIRED)]
+    [MinLength(6, ErrorMessage = ValidationConstants.MIN_LENGTH)]
+    public string Password { get; set; } = default!;
+
+    [Required(ErrorMessage = ValidationConstants.REQUIRED)]
+    [MaxLength(100, ErrorMessage = ValidationConstants.MAX_LENGTH)]
+    public string FirstName { get; set; } = default!;
+
+    [Required(ErrorMessage = ValidationConstants.REQUIRED)]
+    [MaxLength(100, ErrorMessage = ValidationConstants.MAX_LENGTH)]
+    public string LastName { get; set; } = default!;
+
+    public IFormFile? ProfilePicture { get; set; }
+}
